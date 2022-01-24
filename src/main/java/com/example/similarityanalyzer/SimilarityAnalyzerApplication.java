8c328031c34f609cb80f9131c72a4de6f9e30e12
Sotnikov Arkadiy <cotnikoarkady@gmail.com>
2022-01-24 21:03:33 +0400

    Initial commit

---
+++ src/main/java/com/example/similarityanalyzer/SimilarityAnalyzerApplication.java
@@ -0,0 +1,13 @@
+package com.example.similarityanalyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimilarityAnalyzerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimilarityAnalyzerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/similarityanalyzer/SimilarityAnalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.similarityanalyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimilarityAnalyzerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
